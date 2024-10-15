1f3a0d01710d21e1d4f8235957b9849cf26bb59a
Tion Blackmon <tion.blackmon@outlook.com>
2024-10-15 18:41:04 -0500

    Got some elements to render for the home page (all songs).

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ function Home() {
     <div className="container text-center my-4">
       <h1>Home</h1>
       <div className="">
-        <p>Song Name</p>
+        <p> Song Name</p>
         <p>Album Name</p>
         <p>Artist Name</p>
         <p>Song Length</p>
