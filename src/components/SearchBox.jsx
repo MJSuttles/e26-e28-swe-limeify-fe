6d2f4ce13141508ad28630b7a39954582938f2a0
Tion Blackmon <tion.blackmon@outlook.com>
2024-11-02 12:05:31 -0500

    Created a component SearchBox that keeps track of (console.warn) anything the user types. Still a WIP.

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+// import { searchSong } from '../api/songData';
 
 export default function SearchBox() {
+  // const [query, setQuery] = useState('');
+
+  const handleChange = (e) => {
+    const userInput = e.target.value;
+    console.warn(userInput);
+  };
+
   return (
     <div>
-      <h1>SearchBox</h1>
+      <input type="text" name="search" id="search-box" placeholder="search a song" onChange={handleChange} />
     </div>
   );
 }
